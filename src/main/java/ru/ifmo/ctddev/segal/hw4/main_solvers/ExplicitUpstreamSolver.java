f4c0cb0f72d986e66e1978badaca92770f1e1368
DariaYakovleva <dariyakovleva@gmail.com>
2016-11-03 14:40:04 +0300

    implement Explicit upstream solver & implicit upstream solver

--- src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/ExplicitUpstreamSolver.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/ExplicitUpstreamSolver.java
@@ -8,7 +8,15 @@ public class ExplicitUpstreamSolver extends UsualSolver {
 
     @Override
     double[] step(double[] cur, double dx, double dt, double u, double cappa) {
-        // todo: DariaYakovleva
-        return null;
+        int n = cur.length;
+        double[] res = new double[cur.length];
+        double r = cappa * dt / (dx * dx);
+        double s = u * dt / dx;
+        res[0] = (1 - r) * cur[0] + r * cur[1];
+        for (int k = 1; k < n - 1; k++) {
+            res[k] = (r + s) * cur[k - 1] + (1 - 2 * r - s) * cur[k] + r * cur[k + 1];
+        }
+        res[n - 1] = (r + s) * cur[n - 2] + (1 - r - s) * cur[n - 1];
+        return res;
     }
 }
--- src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/ImplicitUpstreamSolver.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/ImplicitUpstreamSolver.java
@@ -1,5 +1,7 @@
 package ru.ifmo.ctddev.segal.hw4.main_solvers;
 
+import ru.ifmo.ctddev.segal.hw4.solvers.TridiagonalSolver;
+
 /**
  * Created by Aleksei Latyshev on 03.11.2016.
  */
@@ -7,7 +9,18 @@ package ru.ifmo.ctddev.segal.hw4.main_solvers;
 public class ImplicitUpstreamSolver extends UsualSolver {
     @Override
     double[] step(double[] cur, double dx, double dt, double u, double cappa) {
-        //todo: DariaYakovleva
-        return null;
+        int n = cur.length;
+        double[][] matrix = new double[cur.length][];
+        double r = cappa * dt / (dx * dx);
+        double s = u * dt / dx;
+        double prevT = -s - r;
+        double curT = 1 + s + 2 * r;
+        double nextT = -r;
+        matrix[0] = new double[]{prevT + curT, nextT};
+        for (int k = 1; k < n - 1; k++) {
+            matrix[k] = new double[]{prevT, curT, nextT};
+        }
+        matrix[n - 1] = new double[]{prevT, curT + nextT};
+        return TridiagonalSolver.solve(matrix, cur);
     }
 }
