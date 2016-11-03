fbfdd2f51f8021a5377f0a4c5833b0ebc8662760
Ignat Loskutov <ignat.loskutov@gmail.com>
2016-11-03 15:19:53 +0300

    Update Solver.java
    Ditto

--- src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/Solver.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/Solver.java
@@ -4,5 +4,5 @@ package ru.ifmo.ctddev.segal.hw4.main_solvers;
  * Created by Aleksei Latyshev on 03.11.2016.
  */
 public interface Solver {
-    double[][] solve(double[] start, double dx, double dt, double u, double cappa, int size);
+    double[][] solve(double[] start, double dx, double dt, double u, double kappa, int size);
 }
