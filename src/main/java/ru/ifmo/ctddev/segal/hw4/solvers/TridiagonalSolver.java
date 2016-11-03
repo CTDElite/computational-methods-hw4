2fb6c35ecb89472d759b54fb662f51dc4b4635fe
alex-700 <alex_700_95@mail.ru>
2016-11-03 13:44:20 +0300

    add task for Wibk (TridiagonalSolver)

---
+++ src/main/java/ru/ifmo/ctddev/segal/hw4/solvers/TridiagonalSolver.java
@@ -0,0 +1,24 @@
+package ru.ifmo.ctddev.segal.hw4.solvers;
+
+/**
+ * Created by Aleksei Latyshev on 03.11.2016.
+ */
+
+public class TridiagonalSolver {
+
+    /**
+     * Solves system.
+     *
+     * @param matrix -- not zero coefficients of matrix
+     * matrix[0].length and matrix[matrix.length - 1].length == 2
+     * others.length = 3
+     *
+     * @param free -- free column
+     * @return solution of this system
+     *
+     */
+    double[] solve(double[][] matrix, double[] free) {
+        return null;
+    }
+
+}
