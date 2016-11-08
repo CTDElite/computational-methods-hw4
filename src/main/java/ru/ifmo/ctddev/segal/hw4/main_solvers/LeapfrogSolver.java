29a36c2832d4ea03f20227628ee3fa13f381bdb8
Ignat Loskutov <loskutov@google.com>
2016-11-08 16:13:43 +0300

    Implement LeapfrogSolver

--- src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/LeapfrogSolver.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/LeapfrogSolver.java
@@ -7,8 +7,22 @@ package ru.ifmo.ctddev.segal.hw4.main_solvers;
 public class LeapfrogSolver implements Solver {
 
     @Override
-    public double[][] solve(double[] start, double dx, double dt, double u, double kappa, int size) {
-        // todo: loskutov
-        return null;
+    public double[][] solve(double[] start, double dx, double dt, double u, double ϰ, int size) {
+        final double[][] ans = new double[size][start.length];
+        double[] prevIter = start;
+        double[] preprevIter = start;
+
+        for (int k = 0; k < size; k++) {
+            for (int n = 0; n < start.length; n++) {
+                final double prevLeft = prevIter[Math.max(0, n - 1)];
+                final double prevRight = prevIter[Math.min(start.length - 1, n + 1)];
+                ans[k][n] = preprevIter[n] - (dx * (prevRight - prevLeft) / dt + 2 * prevIter[n] * (2 * ϰ / dx))
+                                           / (u + 2 * ϰ / dx);
+            }
+            preprevIter = prevIter;
+            prevIter = ans[k];
+        }
+
+        return ans;
     }
 }
