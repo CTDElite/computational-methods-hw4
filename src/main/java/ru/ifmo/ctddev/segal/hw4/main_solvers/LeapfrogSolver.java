bea37e78333e32e8ae8f7427d7a069694c7830fc
Ignat Loskutov <loskutov@google.com>
2016-12-08 02:13:45 +0300

    Get it to work somehow

--- src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/LeapfrogSolver.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw4/main_solvers/LeapfrogSolver.java
@@ -7,21 +7,30 @@ package ru.ifmo.ctddev.segal.hw4.main_solvers;
 public class LeapfrogSolver implements Solver {
 
     @Override
-    public double[][] solve(double[] start, double dx, double dt, double u, double ϰ, int size) {
-        final double[][] ans = new double[size][start.length];
+    public double[][] solve(double[] start, double dx, double dt, double u, double ϰ, int iterations) {
+        final double[][] ans = new double[iterations][start.length];
         double[] prevIter = start;
         double[] preprevIter = start;
 
-        for (int k = 0; k < size; k++) {
+        for (int k = 0; k < iterations; k++) {
             for (int n = 0; n < start.length; n++) {
                 final double prevLeft = prevIter[Math.max(0, n - 1)];
                 final double prevRight = prevIter[Math.min(start.length - 1, n + 1)];
-                ans[k][n] = preprevIter[n] - (dx * (prevRight - prevLeft) / dt + 2 * prevIter[n] * (2 * ϰ / dx))
-                                           / (u + 2 * ϰ / dx);
+                ans[k][n] = dt * preprevIter[n] - (dx * dx * (prevRight - prevLeft) - 4 * dt * ϰ * prevIter[n])
+                                                / (u * dx + 2 * ϰ);
             }
             preprevIter = prevIter;
             prevIter = ans[k];
         }
+//        for (int n = 0; n < iterations; n++) {
+//            for (int k = 0; k < start.length; k++) {
+//                final double prevLeft = prevIter[Math.max(0, k - 1)];
+//                final double prevRight = prevIter[Math.min(start.length - 1, k + 1)];
+//                ans[n][k] = preprevIter[k] - dt * (u*dx*(prevRight - prevLeft) + 2*ϰ*(prevLeft + prevRight - 2*prevIter[k])) / (dx * dx);
+//            }
+//            preprevIter = prevIter;
+//            prevIter = ans[n];
+//        }
 
         return ans;
     }
